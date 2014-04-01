4519f6de6b29adf18b0399a338b0a93d23d0cb48
Herman Schistad <herman.schistad@gmail.com>
2014-04-01 23:57:55 +0200

    Alright then, fix it properly

--- tv.go
+++ tv.go
@@ -305,8 +305,9 @@ func startRecording(sstart, sstop, username, title, channel, transcode string) {
 	}
 
 	// Go assumes UTC, and as we are on Go 1.0 and not 1.1 we dont have ParseInLocation.
-	// Thus we just convert now-time to UTC and add two hours.
-	oslo := time.Now().UTC().Add(time.Duration(7200 * time.Second))
+	// Thus we just convert now-time to UTC and add appropriate hours, based on local time zone.
+	_, inFuture := time.Now().Zone()
+	oslo := time.Now().UTC().Add(time.Duration(time.Duration(inFuture) * time.Second))
 
 	secondsInFuture := start.Sub(oslo).Seconds()
 	duration := stop.Sub(start).Seconds()
